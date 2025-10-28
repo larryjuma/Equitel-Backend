0d348bafdcee0090e53ff1d72fb341734de57137
larryjuma <larryjuma198@gmail.com>
2025-10-28 10:39:24 +0300

    Update MongoDB connection to use environment variable

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+
 
 // Connect mongoose
 const connectDB = require('./config/db');
